eb65b7b2c0376b0eb1888f1d647787e5dea3c78c
Vicent Valls <vizzent@gmail.com>
2024-05-01 07:51:03 +0200

    Improve the look of Splash ant Menu Screen

--- Scenes/Menus/Start/start_menu.gd
+++ Scenes/Menus/Start/start_menu.gd
@@ -1,7 +1,6 @@
 extends CenterContainer
 
 func _ready() -> void:
-	print_debug("Main Menu Ready")
 	_set_panel(1)
 
 func _set_panel(p_no: int)-> void:
@@ -10,22 +9,18 @@ func _set_panel(p_no: int)-> void:
 	$Exit.visible = p_no == 3
 
 func _on_cancel_pressed() -> void:
-	print_debug("Cancel Pressed")
 	_set_panel(1)
 
 
 func _on_exit_pressed() -> void:
-	print_debug("Exit Pressed")
 	get_tree().quit()
 
 
 func _on_exit_menu_pressed() -> void:
-	print_debug("Exit Menu Pressed")
 	_set_panel(3)
 
 
 func _on_options_pressed() -> void:
-	print_debug("Options Pressed")
 	_set_panel(2)
 
 func _on_start_pressed() -> void:
