cd00a75381ecad9fcb406a862b109dd3e7046069
Vicent Valls <vizzent@gmail.com>
2024-05-01 07:44:34 +0200

    Fix blaster to make it compatible with physics running in other thread

--- Scenes/Elements/Shoot/blaster.gd
+++ Scenes/Elements/Shoot/blaster.gd
@@ -1,11 +1,19 @@
 extends Node3D
 
+@onready var raycastShot : RayCast3D = $RayCastShot
+
 func _on_activation(_pickable:XRToolsPickable) -> void:
 	$"../BlasterSound".play()
 	_pickable.get_picked_up_by_controller().trigger_haptic_pulse("haptic", 300.0, 1.0, 0.05, 0)
-	var raycastShot : RayCast3D = $RayCastShot
-	raycastShot.force_raycast_update()
 	if raycastShot.is_colliding():
 		var target = raycastShot.get_collider()
 		if target.is_in_group('target') and target.has_method('hit'):
 			target.hit()
+
+
+func _on_pickable_object_picked_up(pickable):
+	raycastShot.enabled = true
+
+
+func _on_pickable_object_released(pickable, by):
+	raycastShot.enabled = false
