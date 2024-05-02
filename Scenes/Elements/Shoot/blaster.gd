790a83aa05a9e9a5a8dc9e503a1065694b6461b4
Vicent Valls <vizzent@gmail.com>
2024-05-02 17:48:38 +0200

    Add fast cilider as laser effect to give more feedback to the user

--- Scenes/Elements/Shoot/blaster.gd
+++ Scenes/Elements/Shoot/blaster.gd
@@ -1,15 +1,23 @@
 extends Node3D
 
 @onready var raycastShot : RayCast3D = $RayCastShot
+@onready var laser : MeshInstance3D = $Laser
 
 func _on_activation(_pickable:XRToolsPickable) -> void:
 	$"../BlasterSound".play()
 	_pickable.get_picked_up_by_controller().trigger_haptic_pulse("haptic", 300.0, 1.0, 0.05, 0)
+	activateLaser()
+	get_tree().create_timer(0.05).connect("timeout", deactivateLaser)
 	if raycastShot.is_colliding():
 		var target = raycastShot.get_collider()
 		if target.is_in_group('target') and target.has_method('hit'):
 			target.hit()
 
+func activateLaser() -> void:
+	laser.visible = true
+
+func deactivateLaser() -> void:
+	laser.visible = false
 
 func _on_pickable_object_picked_up(pickable):
 	raycastShot.enabled = true
